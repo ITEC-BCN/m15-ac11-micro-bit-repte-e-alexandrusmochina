9f82f8beadb7a5d4f9bf363d4eed06a19770dfd1
Alexandru Smochina <146827365+AlexSmochina@users.noreply.github.com>
2024-11-07 18:38:04 +0100

    Commit

--- main.py
+++ main.py
@@ -1,1 +1,39 @@
+gota = False
+accY = 0
+accX = 0
+Y = 2
+X = 2
+
+def on_forever():
+    global gota, accX, accY, X, Y
+    
+    if not gota and input.button_is_pressed(Button.A):
+        clear_screen()
+        gota = True
+    if input.button_is_pressed(Button.B):
+        gota = False
+    
+    if gota == False:
+        led.plot_bar_graph(input.temperature(), 50)
+    elif gota == True:
+        led.plot(X, Y)
+        basic.pause(50)
+        led.unplot(X, Y)
+        accX = input.acceleration(Dimension.X)
+        accY = input.acceleration(Dimension.Y)
+        if accX <= 150 and X > 0:
+            X = X - 1
+        if accX > 150 and X < 4:
+            X = X + 1
+        if accY <= 150 and Y > 0:
+            Y = Y - 1
+        if accY > 150 and Y < 4:
+            Y = Y + 1
+
+basic.forever(on_forever)
+
+def clear_screen():
+    for y in range(5):
+        for x in range(5):
+            led.unplot(x, y)
 
--- main.ts
+++ main.ts
@@ -1,1 +1,51 @@
+let gota = false
+let accY = 0
+let accX = 0
+let Y = 2
+let X = 2
+basic.forever(function on_forever() {
+    
+    if (!gota && input.buttonIsPressed(Button.A)) {
+        clear_screen()
+        gota = true
+    }
+    
+    if (input.buttonIsPressed(Button.B)) {
+        gota = false
+    }
+    
+    if (gota == false) {
+        led.plotBarGraph(input.temperature(), 50)
+    } else if (gota == true) {
+        led.plot(X, Y)
+        basic.pause(50)
+        led.unplot(X, Y)
+        accX = input.acceleration(Dimension.X)
+        accY = input.acceleration(Dimension.Y)
+        if (accX <= 150 && X > 0) {
+            X = X - 1
+        }
+        
+        if (accX > 150 && X < 4) {
+            X = X + 1
+        }
+        
+        if (accY <= 150 && Y > 0) {
+            Y = Y - 1
+        }
+        
+        if (accY > 150 && Y < 4) {
+            Y = Y + 1
+        }
+        
+    }
+    
+})
+function clear_screen() {
+    for (let y = 0; y < 5; y++) {
+        for (let x = 0; x < 5; x++) {
+            led.unplot(x, y)
+        }
+    }
+}
 
